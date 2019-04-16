164f41a9d69b0ae29e36ecb7f424280333c0ec28
CristianQS <cristian_31teror@hotmail.com>
2019-04-16 19:53:23 +0100

    fix deploy

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,10 +1,4 @@
 module.exports = {
-  lintOnSave: true,
-  configureWebpack: {
-      output: {
-          path: __dirname + "/cool-build"
-      }
-  },
   devServer: {
       disableHostCheck: true
   }
