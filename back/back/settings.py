68cd457d84956f2badc6d77d32830e8e39e17ff0
CristianQS <cristian_31teror@hotmail.com>
2019-04-25 18:17:13 +0100

    update settings

--- back/back/settings.py
+++ back/back/settings.py
@@ -56,7 +56,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'users'
+    'users',
+    'trainings',
+    'activities',
+    'plantrainings'
 ]
 
 MIDDLEWARE = [
