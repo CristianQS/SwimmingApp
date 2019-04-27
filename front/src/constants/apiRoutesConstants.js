6df65e4558f7f320d1d05b12f28fb057e3beaef1
CristianQS <cristianqs31@gmail.com>
2019-04-27 14:12:32 +0100

    Routes

--- front/src/constants/apiRoutesConstants.js
+++ front/src/constants/apiRoutesConstants.js
@@ -1,3 +1,5 @@
 const BASE_URL = 'http://localhost:8000'
 
-export const LOGIN = `${BASE_URL}/auth/login/`
+export const LOGIN = () => `${BASE_URL}/auth/login/`
+export const PLANTRAINING = ()=> `${BASE_URL}/plantrainings`
+export const PLANTRAINING_BY_ID = (id) => `${BASE_URL}/plantrainings/${id}`
