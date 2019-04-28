5ff53efe171d9b0997c007526f7c284f30a6c3ec
CristianQS <cristianqs31@gmail.com>
2019-04-28 02:24:36 +0100

    User apis methods done, and migrations done

--- back/users/urls.py
+++ back/users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import Login
+from .views import Login, CreateUser, FindUserByName, UsersById
 
 urlpatterns = [
     path('auth/login/', Login.as_view()),
+    path('signup/', CreateUser.as_view()),
+    path('users', FindUserByName.as_view()),
+    path('users/<int:id>', UsersById.as_view()),
 ]
