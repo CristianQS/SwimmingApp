374b52ee5f1b87f909e0802c631c5d746e166a9e
CristianQS <cristianqs31@gmail.com>
2019-04-29 21:55:12 +0100

    updated

--- front/src/clients/UserClient.js
+++ front/src/clients/UserClient.js
@@ -2,7 +2,7 @@ import { LOGIN } from '../constants/apiRoutesConstants'
 
 export default function userClient (restClient) {
   function login (user) {
-    return restClient.post(LOGIN, user)
+    return restClient.post(LOGIN(), user)
   }
   function logout (user) {
     //  return restClient.get()
--- front/src/store/actions/TrainingPlanAction.js
+++ front/src/store/actions/TrainingPlanAction.js
@@ -1,9 +1,12 @@
 import { ADD_TRAINING_PLAN, MODIFY_TRAINING_PLAN, 
   DELETE_TRAINING_PLAN } from "../types/TrainingPlanTypes"
+import { planTrainingClient } from '../../clients/factory'
+
 
 export default {
-  [ADD_TRAINING_PLAN] : ({commit}) => {
-
+  [ADD_TRAINING_PLAN] : ({commit},plantraining) => {
+    console.log(plantraining)
+    return planTrainingClient.addPlanTraining(plantraining)
   },
   [MODIFY_TRAINING_PLAN] : ({commit}) => {
 
